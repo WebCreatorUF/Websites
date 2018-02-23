15cea4789271887d6e52c3f307a0559e90ea9ede
WebCreatorUF <35994664+WebCreatorUF@users.noreply.github.com>
2018-02-23 15:48:14 +0100

    Add files via upload

--- Projekt_1/script.js
+++ Projekt_1/script.js
@@ -1,1 +1,23 @@
 // JavaScript Document
+$(document).ready(function () {
+	 'use strict';
+	 $("#sidebar").niceScroll({
+        cursorwidth: 0,
+        cursorborder: 'none'
+	 });
+     $('#dismiss, .overlayside').on('click', function () {
+     	$('#sidebar').removeClass('active');
+		$('.btnNavbar').show('slow');
+		$('.centerimage').show('fast');
+        $('.overlayside').fadeOut();
+	 });
+
+     $('#sidebarCollapse').on('click', function () {
+     	$('#sidebar').addClass('active');
+        $('.overlayside').fadeIn();
+		$('.btnNavbar').toggle('fast');
+		$('.centerimage').toggle('fast');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+	 });
+});
